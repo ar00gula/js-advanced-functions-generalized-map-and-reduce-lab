9629048c9187a0d753921bc60f6a876de469b6ae
ar00gula <arianna.mayer@gmail.com>
2021-01-20 14:01:46 -0500

    commit tree 6612e6e94fd2e0392e3ae472e05eb9d098939cbd

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+function map(sourceArray, myFunc) {
+    let resultArray = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        resultArray.push(myFunc(sourceArray[i]))
+    }
+    return resultArray
+}
+
+
+function reduce(arr, fn, start) {
+    if (start) {
+       arr.forEach(element => {
+            start = fn(element, start)
+        })
+    } else {
+        start = arr[0]
+        for (let i = 1; i < arr.length; i++) {
+            start = fn(arr[i], start)
+        }
+    }
+    return start
+}
